5333ad900c603ebc18abf5550afd4ff66866ae41
Pumidol Leelerdsakulvong <pumidol.l@gmail.com>
2024-02-16 07:50:48 +0000

    Add typography components and useful links to dashboard page

--- app/app/(dashboard)/dashboard/page.tsx
+++ app/app/(dashboard)/dashboard/page.tsx
@@ -1,13 +1,39 @@
 import { auth } from "@/auth";
+import { TypographyH1, TypographyH2 } from "@/components/typographies";
+import Link from "next/link";
 
 export default async function Page() {
   const session = await auth();
   return (
     <main>
-      <div>
-        <h1>Dashboard</h1>
-        <p>Welcome {session!.user.login}</p>
-        <p>Nothing here yet.</p>
+      <div className="flex flex-col gap-4">
+        <div>
+          <TypographyH1>Dashboard</TypographyH1>
+          <p>Welcome {session!.user.login}</p>
+        </div>
+        <div>
+          <TypographyH2>Useful links</TypographyH2>
+          <ul>
+            <li>
+              <Link
+                href="https://intra.42.fr/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                https://intra.42.fr/
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="https://staff.intra.42.fr/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                https://staff.intra.42.fr/
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </main>
   );
