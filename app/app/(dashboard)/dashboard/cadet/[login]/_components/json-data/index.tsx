ae2715c2849289462a591dc58a0ab4efdce376dc
Pumidol Leelerdsakulvong <pumidol.l@gmail.com>
2024-02-17 16:18:32 +0000

    Refactor JSON data component to use button variants

--- app/app/(dashboard)/dashboard/cadet/[login]/_components/json-data/index.tsx
+++ app/app/(dashboard)/dashboard/cadet/[login]/_components/json-data/index.tsx
@@ -1,18 +1,22 @@
-import { Button } from "@/components/ui/button";
+import { buttonVariants } from "@/components/ui/button";
 import {
   Collapsible,
   CollapsibleContent,
   CollapsibleTrigger,
 } from "@/components/ui/collapsible";
+import { cn } from "@/lib/utils";
 import { BracesIcon } from "lucide-react";
 
 export const JsonData = ({ data }: { data: object }) => {
   return (
     <Collapsible>
-      <CollapsibleTrigger className="flex items-center">
-        <Button variant={"ghost"} className="w-24">
-          <BracesIcon size={18} /> JSON
-        </Button>
+      <CollapsibleTrigger
+        className={cn(
+          "flex items-center",
+          buttonVariants({ variant: "ghost" })
+        )}
+      >
+        <BracesIcon size={18} /> JSON
       </CollapsibleTrigger>
       <CollapsibleContent className="bg-slate-100 text-xs font-mono text-slate-600 p-2">
         <pre>{JSON.stringify(data, null, 2)}</pre>
