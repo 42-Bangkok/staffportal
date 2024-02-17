ada569906010ae842f8f855c7d7d688df227a50e
Pumidol Leelerdsakulvong <pumidol.l@gmail.com>
2024-02-17 16:57:51 +0000

    Add wallet form component and update user page

---
+++ app/app/(dashboard)/dashboard/cadet/[login]/_components/wallet-form/types.ts
@@ -0,0 +1,11 @@
+export interface IAddAlt {
+  value: number;
+  user_id: number;
+  login: string;
+}
+
+export interface IWalletForm {
+  currentValue: number;
+  user_id: number;
+  login: string;
+}
--- app/app/(dashboard)/dashboard/cadet/[login]/page.tsx
+++ app/app/(dashboard)/dashboard/cadet/[login]/page.tsx
@@ -1,6 +1,7 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { fetchUser } from "@/lib/intra/users";
 import { JsonData } from "./_components/json-data";
+import { WalletForm } from "./_components/wallet-form";
 
 export default async function Page({ params }: { params: { login: string } }) {
   const { login } = params;
@@ -10,26 +11,31 @@ export default async function Page({ params }: { params: { login: string } }) {
   } catch (e) {
     return <div>Not Found</div>;
   }
+  const walletFormProps = {
+    currentValue: user.wallet,
+    user_id: user.id,
+    login: user.login,
+  };
   return (
     <main>
-      <div>
-        <div className="flex gap-4">
-          <div>
-            <Avatar className="w-24 h-24 rounded">
-              <AvatarImage src={user.image.link} alt={user.login} />
-              <AvatarFallback className="w-24 h-24 rounded">
-                {user.login}
-              </AvatarFallback>
-            </Avatar>
-          </div>
-          <div>
-            <h2>{user.login}</h2>
-            <p>{user.displayname}</p>
-            <p>{user.email}</p>
-          </div>
+      <div className="flex gap-4">
+        <div>
+          <Avatar className="w-24 h-24 rounded">
+            <AvatarImage src={user.image.link} alt={user.login} />
+            <AvatarFallback className="w-24 h-24 rounded">
+              {user.login}
+            </AvatarFallback>
+          </Avatar>
         </div>
-        <JsonData data={user} />
+        <div>
+          <h2>{user.login}</h2>
+          <p>{user.displayname}</p>
+          <p>{user.email}</p>
+          <p>{user.wallet} $Alt</p>
+        </div>
       </div>
+      <WalletForm {...walletFormProps} />
+      <JsonData data={user} />
     </main>
   );
 }
