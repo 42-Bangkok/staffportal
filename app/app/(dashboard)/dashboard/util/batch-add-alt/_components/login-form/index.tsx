b095b21a0ab1c338ca4148c7091516b265c00b9d
Pumidol Leelerdsakulvong <pumidol.l@gmail.com>
2024-02-25 17:26:39 +0000

    Refactor login form component and add commit functionality

--- app/app/(dashboard)/dashboard/util/batch-add-alt/_components/login-form/index.tsx
+++ app/app/(dashboard)/dashboard/util/batch-add-alt/_components/login-form/index.tsx
@@ -36,58 +36,69 @@ export const LoginsForm = (props: ILoginsForm) => {
     const logins = [...new Set(data["logins"].split(" ").filter(Boolean))];
     router.push(`/dashboard/util/batch-add-alt/?logins=${logins.join(",")}`);
   }
+  async function onCommit() {
+    console.log("ids", ids);
+    // console.log("value", props.value);
+  }
 
   return (
-    <Form {...form}>
-      <form
-        onSubmit={form.handleSubmit(onCheckUsers)}
-        className="w-3/4 space-y-6"
-      >
-        <FormField
-          control={form.control}
-          name="logins"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Logins</FormLabel>
-              <FormControl>
-                <Textarea
-                  placeholder="lpumidol aoudin"
-                  className="resize-none"
-                  {...field}
-                />
-              </FormControl>
-              <FormDescription>
-                Separate multiple logins with a space. Duplicated logins are
-                automatically cleared.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <div className="flex flex-col gap-2">
-          <div>
-            <p>
-              DEBUG: the following ids will be used to commit: {ids.join(", ")}
-            </p>
-            {errLogins.length > 0 ? (
-              <p className="text-red-500">
-                logins &quot;{errLogins.join(", ")}&quot; appears invalid,
-                please remove to commit.
+    <div className="flex flex-col gap-2">
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onCheckUsers)}
+          className="w-3/4 space-y-6"
+        >
+          <FormField
+            control={form.control}
+            name="logins"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Logins</FormLabel>
+                <FormControl>
+                  <Textarea
+                    placeholder="lpumidol aoudin"
+                    className="resize-none"
+                    {...field}
+                  />
+                </FormControl>
+                <FormDescription>
+                  Separate multiple logins with a space. Duplicated logins are
+                  automatically cleared.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <div className="flex flex-col gap-2">
+            <div>
+              <p>
+                DEBUG: the following ids will be used to commit:{" "}
+                {ids.join(", ")}
               </p>
-            ) : null}
+              {errLogins.length > 0 ? (
+                <p className="text-red-500">
+                  logins &quot;{errLogins.join(", ")}&quot; appears invalid,
+                  please remove to commit.
+                </p>
+              ) : null}
+            </div>
+            <div className="flex gap-2">
+              <Button className="w-32" type="submit">
+                Check users
+              </Button>
+            </div>
           </div>
-          <div className="flex gap-2">
-            <Button type="submit">Check users</Button>
-            <Button
-              variant="destructive"
-              type="submit"
-              disabled={form.formState.isSubmitting || errLogins.length > 0}
-            >
-              Commit
-            </Button>
-          </div>
-        </div>
-      </form>
-    </Form>
+        </form>
+      </Form>
+      <Button
+        className="w-32"
+        variant="destructive"
+        type="submit"
+        // disabled={form.formState.isSubmitting || errLogins.length > 0}
+        onClick={onCommit}
+      >
+        Commit
+      </Button>
+    </div>
   );
 };
